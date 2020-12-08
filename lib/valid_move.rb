d8616dc5b159faa2b42d641c996f45ba00c71ecf
Keginold Philip <keginoldp@hotmail.com>
2020-12-08 15:43:41 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 def valid_move(board, index)
   if index.between? (0 , 8)
-    if position_taken?(board, index)
+    if position_taken? (board, index)
       true
     else
       false

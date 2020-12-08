37169974603982fa89211ed8e9b08db7ba1c1592
Keginold Philip <keginoldp@hotmail.com>
2020-12-08 15:43:49 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -6,6 +6,7 @@ def valid_move(board, index)
     else
       false
 end
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken? (board, index)
